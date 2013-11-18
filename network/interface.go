a71448bc33898c0d7b1c05c490d7f4ebfbe7b2cb
Lex van Roon <r3boot@r3blog.nl>
2013-11-18 19:05:08 +0100

    Added ResolvConf to Interface

--- network/interface.go
+++ network/interface.go
@@ -7,22 +7,24 @@ import (
 type Interface struct {
     net.Interface
     Type byte
-    WpaSupplicant WpaSupplicant
-    Dhcpcd Dhcpcd
     Link
     RA
     Ip
+    WpaSupplicant WpaSupplicant
+    Dhcpcd Dhcpcd
+    ResolvConf ResolvConf
 }
 
 func InterfaceFactory (intf net.Interface) Interface {
     var i = Interface{
         intf,
         INTF_TYPE_UNKNOWN,
-        WpaSupplicant{Interface: intf.Name},
-        Dhcpcd{Interface: intf.Name},
         Link{Interface: intf},
         RA{Interface: intf.Name},
         Ip{Interface: intf.Name},
+        WpaSupplicant{Interface: intf.Name},
+        Dhcpcd{Interface: intf.Name},
+        ResolvConf{Interface: intf.Name},
     }
 
     i.Type = i.GetType()
